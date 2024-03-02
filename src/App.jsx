36900ad00afcb9908334533f83f903226714c6e2
Rahul Panchal <rm5901960@gmail.com>
2024-03-02 09:40:30 +0530

    Setting up the portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='w-34 h-1/2 text-white bg-red-400'>
+      <div className='w-34 h-1/2 text-white bg-red-400 '>
         hi
       </div>
     </>
