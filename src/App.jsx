1ab210b13fe32177da52af713c9aeddbe2fb7007
Rahul Panchal <rm5901960@gmail.com>
2024-03-02 09:28:16 +0530

    Setting up the portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-34 h-1/2 text-white bg-red-400'>
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
